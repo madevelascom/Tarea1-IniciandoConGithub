d924970b17baaf30065b84d47306c3844ef4ce8c
Madelyne Velasco <m_velasco93@hotmail.com>
2017-10-29 21:26:28 -0500

    Menu

--- taller1.c
+++ taller1.c
@@ -7,7 +7,44 @@ void copy(char to[], char from[]);
 int strlen2(char s[]);
 void squeeze(char s[], int c);
 
+int main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
 
+	max = 0;
+	while ((len = getline2(line, MAXLINE)) > 0)
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+
+	if (max > 0) { /* there was a line */
+		char entrada = '1';
+		char longestcopy[MAXLINE];
+		copy(longestcopy, longest);
+
+		printf("Maxima Cadena: %s\n\n", longest);
+		while (entrada != '3'){
+			
+			printf("=============== MENU ==============\n");
+			printf("1.- Eliminar el caracter 'e' de la cadena. \n"); 
+		  	printf("2.- Hallar longitud de la cadena maxima. \n"); 
+		  	printf("3.- Salir - \n");
+			printf("Ingresa una opcion: ");
+			fflush(stdin);
+			scanf(" %c", &entrada);
+	  		if (entrada == '1'){
+				squeeze(longest, 'e'); 
+				printf("\nMaxima Cadena sin caracter e: %s\n", longest);
+		  	} else if (entrada == '2'){ 
+				printf("\nLongitud de maxima cadena: %d\n", strlen2(longestcopy));				
+	  		}
+		}
+	} return 0;
+}
 
 /* getline: read a line into s, return length*/
 int getline2(char s[],int lim)
